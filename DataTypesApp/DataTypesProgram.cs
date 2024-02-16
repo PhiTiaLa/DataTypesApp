b7dbc83a0121aa0d88c0b9a33e4ee97c9abc5959
lagula.305871 <lagula.305871@LAB-B-PC01>
2024-02-16 09:17:18 +0800

    Add project files.

---
+++ DataTypesApp/DataTypesProgram.cs
@@ -0,0 +1,19 @@
+﻿namespace DataTypesApp
+{
+    internal class DataTypesProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the pieces of apple: ");
+            int apple_Count = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter total price of " + apple_Count + " apples(s): ");
+            double apple_Price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The total price of " + apple_Count + " apples(s) is " + apple_Price);
+            Console.WriteLine("The value of original price is " + apple_Price);
+            int c_apple_Price = (int)apple_Price;
+            Console.WriteLine("The value of converted price is " + c_apple_Price);
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
